e2d39b7469533a980e1702d1c64222c7c6a03160
Antonio Furtado <antoniordf@hotmail.com>
2022-11-15 16:51:23 +0000

    finished wikinimous app skeleton, now just missing design

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = find_article
+  end
+
+  def update
+    @article = find_article
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = find_article
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    Article.find(params[:id])
+  end
+end
